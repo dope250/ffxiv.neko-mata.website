ea2157fa12119b9387ac00910f7816e2aa89d16d
Dominic <dope250@electronicping.net>
2024-09-11 12:13:27 +0200

    Fix: build.sh for linux

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 git pull
 set -o allexport && source .env && set +o allexport
-TodayUnix=$(date -v-2d +%s)
+#TodayUnix=$(date -v-2d +%s)
+TodayUnix=$(date +%s -d "2 days ago")
 curl --request GET --url https://raid-helper.dev/api/v3/servers/$RAIDHELPER_SERVER_ID/events --header "Authorization: $RAIDHELPER_API" --header "StartTimeFilter: $TodayUnix" > data/events.json
 hugo -D
 swa deploy --env production
